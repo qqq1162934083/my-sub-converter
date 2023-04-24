47eb6c3d993076b96cf928eb0be6ced485ea88fc
刘华栋 <1162934083@qq.com>
2023-04-24 22:17:54 +0800

    springboot项目初始化

---
+++ src/main/java/my/sub/MySubConverterApplication.java
@@ -0,0 +1,13 @@
+package my.sub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySubConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySubConverterApplication.class, args);
+    }
+
+}
